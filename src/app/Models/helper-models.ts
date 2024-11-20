c14e9d6841d509a78ec7ed8b7006625cc97dc39a
mlidon <lidonmarc@gmail.com>
2024-11-20 11:32:10 +0100

    Menu header adapted

--- src/app/Models/helper-models.ts
+++ src/app/Models/helper-models.ts
@@ -1,9 +1,20 @@
 export class CardSkills {
-   title?:       string;
-   description?: string;
-   urlImage?:    string;
+  title?:       string;
+  description?: string;
+  urlImage?:    string;
 
    constructor(values: Object = {}) {
      (<any>Object).assign(this, values);
    }
- }
+}
+
+export class MenuHeader {
+  title?:       string;
+  link?:        string;
+  fragment?:    string;
+  icon?:        string;
+
+  constructor(values: Object = {}) {
+    (<any>Object).assign(this, values);
+  }
+}
--- src/app/shared/components/home/header-home/header-home.component.ts
+++ src/app/shared/components/home/header-home/header-home.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import {MatMenuModule} from '@angular/material/menu';
+import { MenuHeader } from '../../../../Models/helper-models';
+
+
 @Component({
   selector: 'shared-header-home',
   standalone: true,
-  imports: [RouterLink,MatToolbarModule,MatIconModule,MatButtonModule],
+  imports: [RouterLink,MatToolbarModule,MatIconModule,MatButtonModule,MatMenuModule],
   templateUrl: './header-home.component.html',
   styleUrl: './header-home.component.scss'
 })
-export class HeaderHomeComponent {
 
+export class HeaderHomeComponent implements OnInit {
+  isMobile?: boolean;
+  itemsMenu?: MenuHeader[]=[
+    {title:'Home',link:'',fragment:'',icon:'home'},
+    {title:'Skills',link:'home/',fragment:'skills',icon:'share'},
+    {title:'About Me',link:'home/',fragment:'aboutme',icon:'mood'},
+    {title:'Books',link:'home/',fragment:'books',icon:'book_2'},
+    {title:'Contact',link:'home/',fragment:'contact',icon:'person_add'},
+    {title:'login',link:'login',fragment:'',icon:'login'},
+  ];
+  
+  constructor(private breakpointObserver: BreakpointObserver){}
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe([Breakpoints.Handset]).subscribe({
+      next:(result=>{
+        this.isMobile = result.matches;
+      })
+    })
+  }
+
+  toogleMenu(){
+    
+  }
 }
